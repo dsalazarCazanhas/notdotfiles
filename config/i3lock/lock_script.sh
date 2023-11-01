4d50e77e9e7f51d55d4bd71817ac42f67ad9a3a0
dsalazarCazanhas <dsalazarcazanhas@gmail.com>
2023-10-31 21:11:11 -0400

    Update to lock script

--- config/i3lock/lock_script.sh
+++ config/i3lock/lock_script.sh
@@ -5,10 +5,10 @@ revert() {
   xset dpms 0 0 0
 }
 trap revert HUP INT TERM
-xset +dpms dpms 0 30 5
+xset +dpms dpms 5 5 5
 scrot -d 1 /tmp/locking_screen.png
 convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
 convert -composite /tmp/screen_blur.png ~/.config/i3lock/rick.png -geometry -20x1200 /tmp/screen.png
 convert -composite /tmp/screen.png ~/.config/i3lock/glados_edited.png -geometry +550 /tmp/screen.png
-i3lock -i /tmp/screen.png -te
+i3lock  -n -i /tmp/screen.png -tue
 revert
