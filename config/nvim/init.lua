295aeab18ab593a0142b97cf8396827a7cb2b46d
Dayron Salazar <dsalazarcazanhas@gmail.com>
2024-02-28 17:26:11 -0500

    changed nvim configuration

--- config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
-require("gh0st.plugins-setup")
-require("gh0st.core.options")
-require("gh0st.core.keymaps")
-require("gh0st.core.colorscheme")
-require("gh0st.plugins.nvim-tree")
-require("gh0st.plugins.lualine")
-require("gh0st.plugins.colorizer")
