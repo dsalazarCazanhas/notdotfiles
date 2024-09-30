4ebe82de34c668dfeaae215572bc3a6c77484216
Dayron <dayron@klinklin.ai>
2024-09-29 20:21:42 -0400

    Added a fancy title and fixed some logic in the code

--- install.py
+++ install.py
@@ -2,20 +2,46 @@ import subprocess
 import os
 import time
 
-packages = ['zip', 'unzip', 'alacritty', 'tmux', 'git', 'oh-my-zsh-git', 'dbus-glib', 'nmap-git', 'wireshark-git', 'virtualbox-bin', 'virtualbox-bin-guest-iso', 'virtualbox-host-modules-arch', 'diffutils', 'util-linux', 'less', 'debugedit', 'fakeroot', 'gzip', 'binutils', 'bat', 'devtools', 'lsd']
-cybersec_path = "/user/share/cybersec"
+packages = ['zip', 'unzip', 'alacritty', 'tmux', 'kitty', 'git', 'oh-my-zsh-git',
+            'dbus-glib', 'nmap-git', 'wireshark-git', 'virtualbox-bin',
+            'virtualbox-bin-guest-iso', 'virtualbox-host-modules-arch',
+            'diffutils', 'util-linux', 'less', 'most', 'debugedit', 'fakeroot',
+            'gzip', 'binutils', 'bat', 'devtools', 'lsd', 'cowsay', 'toilet']
 install_dir = os.path.dirname(__file__)
 home_path = os.getenv('HOME')
 config_dir = f"{home_path}/.config"
-cur_user = subprocess.check_output(['id', '-un']).decode('utf-8')
+cur_user = subprocess.check_output(['id', '-un']).decode('utf-8').strip()
 cur_user_uid = int(subprocess.check_output(['id', '-u']))
 cur_user_gid = int(subprocess.check_output(['id', '-g']))
-
-
-def stop_on_error(excep: Exception, func_name: str):
-    print(f"The program will stop because there was an error"
-          f"on function '{func_name}' with error: {excep}")
-    exit()
+current_os = subprocess.check_output(["sed", "-n", "-e", "2p", "/etc/os-release"]).decode('utf-8').strip()
+cyber_path = f"{home_path}/.local/share/cybersec"
+start_title = """
+***************************************************************************
+*                                                                         *
+*    OOOOO   H   H   H   H   H   H      SSSSS  W   W  EEEEE  EEEEE  TTTTT *
+*    O   O   H   H   H   H   H   H      S      W   W  E      E        T   *
+*    O   O   HHHHH   HHHHH   HHHHH      SSSSS  W W W  EEEEE  EEEEE    T   *
+*    O   O   H   H   H   H   H   H          S  W W W  E      E        T   *
+*    OOOOO   H   H   H   H   H   H      SSSSS  W   W  EEEEE  EEEEE    T   *
+*                                                                         *
+*                                                                         *
+*                                                    ""                   *
+*    SSSSS  CCCCC  RRRRR  IIIII  PPPPP  TTTTT        "" OOOOO             *
+*    S      C      R   R    I    P   P    T          "" O   O             *
+*    SSSSS  C      RRRR     I    PPPPP    T             O   O             *
+*        S  C      R   R    I    P        T             O   O             *
+*    SSSSS  CCCCC  R    R IIIII  P        T             OOOOO             *
+*                                                                         *
+*                                                                         *
+*                                                                         *
+*    M   M  IIIII  N   N  EEEEE  EEEEE  EEEEE  EEEEE  ! ! !               *
+*    MM MM    I    NN  N  E      E      E      E      ! ! !               *
+*    M M M    I    N N N  EEEEE  EEEEE  EEEEE  EEEEE  ! ! !               *
+*    M   M    I    N  NN  E      E      E      E      ! ! !               *
+*    M   M  IIIII  N   N  EEEEE  EEEEE  EEEEE  EEEEE  0 0 0               *
+*                                                                         *
+***************************************************************************
+"""
 
 
 def reboot():
@@ -53,59 +79,49 @@ def install_pkg(pkg: str):
         stop_on_error(e, install_pkg.__name__)
 
 
-def check_process_status(process_name: str):
-    try:
-        subprocess.check_output(['pgrep', process_name])
-        time.sleep(2)
-        print(f"The process {process_name} is running, continuing")
-        return True
-    except subprocess.CalledProcessError:
-        print(f"The process {process_name} is not running")
-        return False
-
-
 def init():
     time.sleep(2)
     print("Executing first system upgrade.")
     time.sleep(1)
-    subprocess.run(['sudo', 'pacman-key', '--init'])
-    subprocess.run(['sudo', 'pacman-key', '--populate'])
+    subprocess.run(["sudo", "pacman-key", "--init"])
+    subprocess.run(["sudo", "pacman-key", "--populate"])
     try:
-        subprocess.run(['sudo', 'pacman', '-Syyu', '--noconfirm'], check=True)
+        subprocess.run(["sudo", "pacman", "-Syyu", "--noconfirm"], check=True)
         time.sleep(3)
         reboot()
-    except Exception as e:
-        stop_on_error(e)
+    except Exception:
+        raise Exception("There was a problem in the first system upgrade.")
 
 
 def install_paru():
+    time.sleep(2)
+    print("Installing the beloved PARU...")
+    time.sleep(1)
     if subprocess.check_output(['git', '-v']):
         if subprocess.check_output(['paru', '-V']):
             print("Paru is already installed.")
+            time.sleep(2)
             return True
         print("Git is installed, cloning paru...")
         time.sleep(2)
         try:
-            subprocess.run(['git', 'clone', 'https://aur.archlinux.org/paru-bin', '/tmp/paru'])
-            os.system("cd /tmp/paru; makepkg -sic")
+            subprocess.run(["git", "-C", "/tmp/", "clone", "https://aur.archlinux.org/paru-bin"])
+            subprocess.run(["makepkg", "-sicD", "/tmp/paru-bin"])
             time.sleep(2)
             if subprocess.check_output(['paru', '-V']):
-                print("Paru was installed successfully")
+                print("Paru was installed correctly.")
+                time.sleep(2)
                 return True
-            else:
-                return "Paru was not installed correctly"
-        except:
-            raise Exception("Problema con tu primo el paru")
-            return False
+        except Exception:
+            raise
     else:
         print("Git isn't installed, it will be installed")
         try:
-            if install_pkg('git'):
-                install_paru()
-        except:
-            raise Exception("There was a problem installing git")
-            return False
-        return False
+            install_pkg('git')
+            if install_paru():
+                return True
+        except Exception:
+            raise
 
 
 def set_tor():
@@ -113,9 +129,8 @@ def set_tor():
     try:
         subprocess.run(['paru', '-S', 'proxychains', 'tor', 'torsocks', '--noconfirm', '--needed'])
         return True
-    except:
-        raise Exception("Problemas en el metodo set tor")
-        return False
+    except Exception:
+        raise Exception("There was a problem installing TOR")
 
 
 def install_oh_my_zsh():
@@ -129,54 +144,50 @@ def install_oh_my_zsh():
 
 def install_fzf():
     try:
-        os.system("git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install")
+        subprocess.run(["git", "clone --depth 1", "https://github.com/junegunn/fzf.git", "~/.fzf", "&&", "~/.fzf/install"])
         return True
-    except:
-        return False
+    except Exception:
+        raise Exception("There was an error installing fzf")
 
 
 def install_packages():
+    print("Let's install the things.")
+    time.sleep(2)
     try:
         subprocess.run(['sudo', 'systemctl', 'restart', 'tor'])
     except subprocess.CalledProcessError:
-        print("Tor isn't running")
-        return 1
-    if check_process_status('tor'):
-        try:
-            subprocess.run(["proxychains4", "paru", "-S", "--noconfirm", ] \
-                           + packages, check=True)
-            return True
-        except subprocess.CalledProcessError:
-            print("There was an error downloading packages")
-            return False
-    else:
-        raise Exception("There is a problem with tor process")
-        return False
+        raise
+    try:
+        subprocess.check_output(["proxychains4", "paru", "-S", "--noconfirm", *packages])
+        return True
+    except subprocess.CalledProcessError as exc:
+        print(f"There was an error downloading packages: {exc}")
+        raise exc
+    except Exception:
+        raise
 
 
-def main():
-    print("Let's go")
-    # init()
+def prepare():
     time.sleep(1)
     print("Preparing folders...")
-    time.sleep(2)
-    if not os.path.exists('/usr/share/cybersec'):
-        print("Cybersec path doesn't exists, creating it...")
+    time.sleep(1)
+    if not os.path.exists(cyber_path):
+        print("Creating a folder to store cybersec tools.")
         time.sleep(2)
         try:
-            subprocess.run(['sudo', 'mkdir', '/usr/share/cybersec'], check=True)
-            subprocess.run(['sudo', 'chown', f"{cur_user_uid}:{cur_user_gid}", '/usr/share/cybersec'], check=True)
-            return True
+            subprocess.run(["mkdir", "-p", f"{cyber_path}"], check=True)
         except subprocess.CalledProcessError:
-            raise Exception(f"There was an error executing sudo")
-            return False
+            raise
+        except Exception:
+            raise
     else:
         print("Cybersec path already exists continuing")
         time.sleep(2)
         pass
     time.sleep(2)
-    #print("Installing ohmyzsh")
-    #install_oh_my_zsh()
+    print("Installing ohmyzsh")
+    try:
+        install_oh_my_zsh()
     #print("Installing FZF")
     #install_fzf()
     #time.sleep(2)
@@ -186,24 +197,24 @@ def main():
     #os.system(f"mkdir {cybersec_path}/cyberchef")
     #os.system(f"curl https://gchq.github.io/CyberChef/CyberChef_v10.8.2.zip -o {cybersec_path}/cyberchef/cyber.zip")
     #os.system(f"cd {cybersec_path}/cyberchef && unzip cyber.zip")
-    print("Downloading paru")
-    if install_paru():
+    try:
+        install_paru()
+        time.sleep(2)
+    except Exception:
+        raise
+    try:
         set_tor()
         time.sleep(2)
-        try:
-            install_packages()
-            print("Se instalo to...")
-            pass
-        except:
-            time.sleep(2)
-            raise Exception("Problemas descargando los paquetes.")
-            return False
-        time.sleep(2)
-    else:
-        print("There is an error on function install_paru()")
-        return 1
-    time.sleep(2)
+    except Exception:
+        raise
+    try:
+        install_packages()
+    except Exception:
+        raise Exception("Problemas descargando los paquetes.")
+    except subprocess.CalledProcessError:
+        raise
     print("Setting configuration files...")
+    time.sleep(2)
     os.system(f"cp -rf {install_dir}/zsh_powerlevel/p10k.zsh {home_path}/.p10k.zsh;"
               f"cp -rf {install_dir}/zsh_powerlevel/zshrc {home_path}/.zshrc;"
               f"cp -rf {install_dir}/config/alacritty.toml {home_path}/.alacritty.toml;"
@@ -218,14 +229,21 @@ def main():
 
 
 def sweet_script_o_mine():
-    print(f"Starting\
-    Checking configs for {cur_user} user:\
-        Install directory: {install_dir}\
-        Home directory: {home_path}\
-        Config user directory: {config_dir}")
+    print(f"""
+    Starting...
+    Current system: {current_os}
+    Checking configs for {cur_user}:
+        Install directory: {install_dir}
+        
+        Home directory: {home_path}
+        
+        Config user directory: {config_dir}
+
+        Packages to install: {packages}
+    """)
     time.sleep(3)
-    print(f"The program needs that everything is updated, "
-          f"do you want me to do it?")
+    print("The script needs that everything is updated, "
+          "do you want me to do it?(It'll need root privileges)")
     x = input("(y/n): ").lower()
     if x == 'y':
         try:
@@ -234,27 +252,60 @@ def sweet_script_o_mine():
         except Exception as e:
             stop_on_error(e)
     elif x == 'n':
-        try:
-            main()
-        except:
-            raise Exception("Something went wrong")
+        print("OK, let's install then the things that matters. \\o/ ...")
+        time.sleep(1)
+        prepare()
     else:
-        print("Are you blind? Just type y or n")
+        print("Are you blind? Just type Y or N,\
+              lets start over.")
+        sweet_script_o_mine()
 
 
-# Checking if the process is being executed after a reboot
-if subprocess.run(['crontab', '-l'], check=True):
-    subprocess.run(['crontab', '-r'])
-    main()
-else:
-    # Start the complete process #
-    print("**** OH...SWEET SCRIPT O' MINE!! ****")
+def main():
+    time.sleep(1)
+    print("Mmmm... Let me think.")
     time.sleep(2)
-    if cur_user == 'root':
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    print('.')
+    time.sleep(1)
+    #if 'arch' not in current_os.lower():
+    #    raise Exception(f"Sorry -->{current_os}<-- is not supported \nOther Systems that's not Arch Linux are not supported.....Yet :/ ")
+    if os.geteuid() == 0:
         raise Exception("This script can't be run as root")
     else:
+        print("Checking if the process is being executed after a reboot")
+        time.sleep(3)
         try:
-            sweet_script_o_mine()
-        except Exception as e:
-            print("There was an error executing the sweet script")
-            exit(51)
+            if str(subprocess.run(['crontab', '-l']))[0] == 'n':
+                print("Reboot completed, resuming...")
+                time.sleep(3)
+                subprocess.run(['crontab', '-r'])
+            else:
+                # Start the complete process #
+                time.sleep(2)
+                print("Starting for the first time, lets go!!")
+                time.sleep(3)
+                print(start_title)
+                time.sleep(2)
+                try:
+                    sweet_script_o_mine()
+                except Exception:
+                    raise
+        except Exception:
+            raise
+        except KeyboardInterrupt:
+            raise
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    except KeyboardInterrupt:
+        print("\nOK, that's fine, you can stop this at any time. The SUN will be PRAISED anyways \\o/.")
+
+
